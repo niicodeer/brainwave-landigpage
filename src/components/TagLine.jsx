972cf406d34f49802fb4a62d625593aab9f71a8f
niicodeer <nicoradin.job@gmail.com>
2024-03-05 14:09:57 -0300

    pricing section layout

---
+++ src/components/TagLine.jsx
@@ -0,0 +1,13 @@
+import brackets from "../assets/svg/Brackets";
+
+function TagLine({ className, children }) {
+  return (
+    <div className={`tagline flex items-center ${className || ""}`}>
+      {brackets("left")}
+      <div className="mx-3 text-n-4">{children}</div>
+      {brackets("right")}
+    </div>
+  );
+}
+
+export default TagLine;
