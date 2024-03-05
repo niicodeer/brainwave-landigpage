f64807378aad1474c09a19df85771dbf65fa895d
niicodeer <nicoradin.job@gmail.com>
2024-03-04 21:48:25 -0300

    bento box section layout

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,7 +1,8 @@
-function Heading({ className, title }) {
+function Heading({ className, title, text }) {
   return (
     <div className={`${className} max-w-[50rem] mx-auto mb-12 lg:mb-20`}>
       {title && <h2 className="h2">{title}</h2>}
+      {text && <p className="bodey-2 mt-4 text-n-4">{text}</p>}
     </div>
   );
 }
